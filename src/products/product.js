621148000d32f95ed451c4e668868ec1477e69f0
OlwethuB <olwethu.a.bhiya@gmail.com>
2023-05-30 14:49:07 +0200

    structure

--- src/products/product.js
+++ src/products/product.js
@@ -1,3 +1,55 @@
+
+// let product1 = {
+//     name: 'Book1',
+//     author: 'An Author',
+//     price: 'R926'
+// };
+// let product2 = {
+//     name: 'Book1',
+//     author: 'An Author',
+//     price: 'R926'
+// };
+// let product3 = {
+//     name: 'Book1',
+//     image: 'https://i.postimg.cc/0NfXStwZ/Closed-Book-Clip-Art-Black-and-White.jpg',
+//     author: 'An Author',
+//     price: 'R926'
+// };
+// let product4 = {
+//     name: 'Book1',
+//     author: 'An Author',
+//     price: 'R926'
+// };
+
+
+// let product5 = {
+//     name: 'Book1',
+//     author: 'An Author',
+//     price: 'R926'
+// };
+// let product6 = {
+//     name: 'Book1',
+//     author: 'An Author',
+//     price: 'R926'
+// };
+// let product7 = {
+//     name: 'Book1',
+//     author: 'An Author',
+//     price: 'R926'
+// };
+// let product8 = {
+//     name: 'Book1',
+//     author: 'An Author',
+//     price: 'R926'
+// };
+
+// let produce = document.getElementById("Produce");
+
+// let products =[product1, product2, product3, product4, product5, product6, product7, product8];
+// products.forEach(product => produce.innerHTML += "<br>" + Object.values(product).join(" "));
+
+
+  
 // Array of products
 const products = [
     {
@@ -70,4 +122,4 @@ const products = [
   products.forEach(product => {
     const productElement = createProductElement(product);
     productsContainer.appendChild(productElement);
-  });    
+  });
